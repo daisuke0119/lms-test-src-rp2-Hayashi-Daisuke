2448bc9574726274d1e7ec80d00158394cecd55e
edu <edu@B65DS.mshome.net>
2026-03-23 17:51:07 +0900

    add//Case04.javaによくある質問画面への画面遷移のテストコードを追記
    ・タイトルを取得し画面遷移の認証を行う。
    期待値：ログイン｜LMS
    
    ・正しいIDとパスワードでログインを行い、遷移先の画面のタイトルを取得、検証する。
    期待値：コース詳細｜LMS
    
    ・コース詳細画面からヘルプ画面に遷移、タイトルを取得、検証する。
    期待値：ヘルプ｜LMS
    
    ・ヘルプ画面から別タブでよくある質問画面に遷移、タイトルを取得、検証する。
    期待値：よくある質問｜LMS

--- test_code_creation_hayashidaisuke/src/test/java/jp/co/sss/lms/ct/f02_faq/Case04.java
+++ test_code_creation_hayashidaisuke/src/test/java/jp/co/sss/lms/ct/f02_faq/Case04.java
@@ -1,6 +1,7 @@
 package jp.co.sss.lms.ct.f02_faq;
 
 import static jp.co.sss.lms.ct.util.WebDriverUtils.*;
+import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
@@ -9,6 +10,7 @@ import org.junit.jupiter.api.MethodOrderer.OrderAnnotation;
 import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestMethodOrder;
+import org.openqa.selenium.By;
 
 /**
  * 結合テスト よくある質問機能
@@ -35,28 +37,88 @@ public class Case04 {
 	@Order(1)
 	@DisplayName("テスト01 トップページURLでアクセス")
 	void test01() {
-		// TODO ここに追加
+
+		//ログインページを表示
+		goTo("http://localhost:8080/lms");
+
+		//タイトルが正しいか検証
+		assertEquals("ログイン | LMS", webDriver.getTitle());
+
+		//スクリーンショットを撮影
+		getEvidence(new Object() {
+		});
 	}
 
 	@Test
 	@Order(2)
 	@DisplayName("テスト02 初回ログイン済みの受講生ユーザーでログイン")
 	void test02() {
-		// TODO ここに追加
+
+		//ログインIDを入力
+		webDriver.findElement(By.id("loginId")).sendKeys("StudentAA01");
+
+		//パスワードを入力
+		webDriver.findElement(By.id("password")).sendKeys("PasswordAA01");
+
+		//ログインボタンをクリック
+		webDriver.findElement(By.cssSelector(".btn.btn-primary")).click();
+
+		//タイトルが正しいか検証
+		assertEquals("コース詳細 | LMS", webDriver.getTitle());
+
+		//スクリーンショットを撮影
+		getEvidence(new Object() {
+		});
 	}
 
 	@Test
 	@Order(3)
 	@DisplayName("テスト03 上部メニューの「ヘルプ」リンクからヘルプ画面に遷移")
 	void test03() {
-		// TODO ここに追加
+
+		//「機能」をクリックしてドロップダウンリストを表示
+		webDriver.findElement(By.className("dropdown-toggle")).click();
+
+		//「ヘルプ」をクリック
+		webDriver.findElement(By.partialLinkText("ヘルプ")).click();
+
+		//タイトルが正しいか検証
+		assertEquals("ヘルプ | LMS", webDriver.getTitle());
+
+		//スクリーンショットを撮影
+		getEvidence(new Object() {
+		});
 	}
 
 	@Test
 	@Order(4)
 	@DisplayName("テスト04 「よくある質問」リンクからよくある質問画面を別タブに開く")
 	void test04() {
-		// TODO ここに追加
+
+		//現在のウィンドウハンドルを取得
+		String presentPage = webDriver.getWindowHandle();
+
+		//「よくある質問」をクリック
+		webDriver.findElement(By.partialLinkText("よくある質問")).click();
+
+		//別タグが開かれているか検証
+		int pageQuantity = webDriver.getWindowHandles().size();
+		assertEquals(2, pageQuantity);
+
+		//新しいタブに切り替え
+		for (String newPage : webDriver.getWindowHandles()) {
+			if (!presentPage.contentEquals(newPage)) {
+				webDriver.switchTo().window(newPage);
+				break;
+			}
+		}
+
+		//新しいタブでタイトルが正しいか検証
+		assertEquals("よくある質問 | LMS", webDriver.getTitle());
+
+		//スクリーンショットを撮影
+		getEvidence(new Object() {
+		});
 	}
 
 }
