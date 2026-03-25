55405b0531a466d35f5a8765caa4568c9bfc46f6
edu <edu@B65DS.mshome.net>
2026-03-25 11:34:03 +0900

    add//Case08.javaにレポート(週報)修正のテストコードを追記
    ・タイトルを取得し画面遷移の認証を行う。
    期待値：ログイン｜LMS
    
    ・正しいIDとパスワードでログインを行い、遷移先の画面のタイトルを取得、検証する。
    期待値：コース詳細｜LMS
    
    ・コース詳細画面から2022年10月2日(日)の「詳細」ボタンを押下し、正しい画面遷移が行われているかタイトルを取得し検証
    期待値：セクション詳細｜LMS
    
    ・「提出済み週報を確認する」ボタンを押下し、正しい画面遷移が行われているかタイトルを取得、検証する。
    期待値：レポート登録｜LMS
    
    ・報告内容を修正し、週報の提出を行う。
    正しい画面遷移が行われているかタイトルを取得し、検証
    期待値：セクション詳細 | LMS
    
    ・ヘッダーの「ようこそ受講生AA1さん」リンクをクリックし、正しく画面遷移が行われているかタイトルを取得し、検証
    期待値：ユーザー詳細
    
    ・2022年10月2日(日)のレポートの「詳細」ボタンを押下
    正しく画面遷移が行われているかタイトルを取得、検証し、修正した内容に正しく更新されているか確認
    期待値：レポート詳細 | LMS
    修正内容：
    今週も頑張りました！
    来週も頑張ります！

--- test_code_creation_hayashidaisuke/src/test/java/jp/co/sss/lms/ct/f03_report/Case08.java
+++ test_code_creation_hayashidaisuke/src/test/java/jp/co/sss/lms/ct/f03_report/Case08.java
@@ -1,6 +1,7 @@
 package jp.co.sss.lms.ct.f03_report;
 
 import static jp.co.sss.lms.ct.util.WebDriverUtils.*;
+import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
@@ -9,6 +10,8 @@ import org.junit.jupiter.api.MethodOrderer.OrderAnnotation;
 import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestMethodOrder;
+import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
 
 /**
  * 結合テスト レポート機能
@@ -35,49 +38,138 @@ public class Case08 {
 	@Order(1)
 	@DisplayName("テスト01 トップページURLでアクセス")
 	void test01() {
-		// TODO ここに追加
+
+		//ログインページを表示
+		goTo("http://localhost:8080/lms");
+
+		//タイトルが正しいか検証
+		assertEquals("ログイン | LMS", webDriver.getTitle());
+
+		//スクリーンショットを撮影
+		getEvidence(new Object() {
+		});
 	}
 
 	@Test
 	@Order(2)
 	@DisplayName("テスト02 初回ログイン済みの受講生ユーザーでログイン")
 	void test02() {
-		// TODO ここに追加
+
+		//ログインIDを入力
+		webDriver.findElement(By.id("loginId")).sendKeys("StudentAA01");
+
+		//パスワードを入力
+		webDriver.findElement(By.id("password")).sendKeys("PasswordAA01");
+
+		//ログインボタンをクリック
+		webDriver.findElement(By.cssSelector(".btn.btn-primary")).click();
+
+		//タイトルが正しいか検証
+		assertEquals("コース詳細 | LMS", webDriver.getTitle());
+
+		//スクリーンショットを撮影
+		getEvidence(new Object() {
+		});
 	}
 
 	@Test
 	@Order(3)
 	@DisplayName("テスト03 提出済の研修日の「詳細」ボタンを押下しセクション詳細画面に遷移")
 	void test03() {
-		// TODO ここに追加
+
+		//コース詳細画面で2022年10月2日(日)の「詳細」ボタンを押下
+		webDriver.findElements(By.cssSelector(".btn.btn-default")).get(2).click();
+
+		//タイトルが正しいか検証
+		assertEquals("セクション詳細 | LMS", webDriver.getTitle());
+
+		//スクリーンショットを撮影
+		getEvidence(new Object() {
+		});
 	}
 
 	@Test
 	@Order(4)
 	@DisplayName("テスト04 「確認する」ボタンを押下しレポート登録画面に遷移")
 	void test04() {
-		// TODO ここに追加
+
+		//「提出済み週報を確認する」ボタンが表示される箇所までページをスクロール
+		((JavascriptExecutor) webDriver).executeScript("window.scrollBy(0,1000)");
+
+		//「提出済み週報を確認する」ボタンを押下
+		webDriver.findElements(By.cssSelector(".btn.btn-default")).get(3).click();
+
+		//タイトルが正しいか検証
+		assertEquals("レポート登録 | LMS", webDriver.getTitle());
+
+		//スクリーンショットを撮影
+		getEvidence(new Object() {
+		});
 	}
 
 	@Test
 	@Order(5)
 	@DisplayName("テスト05 報告内容を修正して「提出する」ボタンを押下しセクション詳細画面に遷移")
 	void test05() {
-		// TODO ここに追加
+
+		//「提出する」ボタンが表示される箇所までページをスクロール
+		((JavascriptExecutor) webDriver).executeScript("window.scrollBy(0,1000)");
+
+		//テキストボックスの内容を消去する
+		webDriver.findElement(By.id("content_1")).clear();
+
+		//テキストボックスに修正済みの所感の内容を入力
+		String fixedReport = "今週も頑張りました！\n来週も頑張ります！";
+		webDriver.findElement(By.id("content_1")).sendKeys(fixedReport);
+
+		//「提出する」ボタンを押下
+		webDriver.findElement(By.cssSelector(".btn.btn-primary")).click();
+
+		//正しく画面遷移が行われているかタイトルを取得して検証
+		assertEquals("セクション詳細 | LMS", webDriver.getTitle());
+
+		//スクリーンショットを撮影
+		getEvidence(new Object() {
+		});
 	}
 
 	@Test
 	@Order(6)
 	@DisplayName("テスト06 上部メニューの「ようこそ○○さん」リンクからユーザー詳細画面に遷移")
 	void test06() {
-		// TODO ここに追加
+
+		//上部メニューの「ようこそ受講生AA1さん」のリンクをクリック
+		webDriver.findElement(By.tagName("small")).click();
+
+		//タイトルを取得して検証
+		assertEquals("ユーザー詳細", webDriver.getTitle());
+
+		//スクリーンショットを撮影
+		getEvidence(new Object() {
+		});
 	}
 
 	@Test
 	@Order(7)
 	@DisplayName("テスト07 該当レポートの「詳細」ボタンを押下しレポート詳細画面で修正内容が反映される")
 	void test07() {
-		// TODO ここに追加
+
+		//レポートの一覧が表示される箇所までページをスクロール
+		((JavascriptExecutor) webDriver).executeScript("window.scrollBy(0,1000)");
+
+		//先ほど編集した2022年10月2日(日)の項目のレポートの「詳細」ボタンをクリック
+		webDriver.findElements(By.cssSelector(".btn.btn-default")).get(6).click();
+
+		//正しく画面遷移が行われているかタイトルを取得して検証
+		assertEquals("レポート詳細 | LMS", webDriver.getTitle());
+
+		//表示されているレポートの所感の内容が修正済みの内容に更新されているか検証
+		String expectedReportText = "今週も頑張りました！\n来週も頑張ります！";
+		assertEquals(expectedReportText, webDriver.findElements(By.tagName("td")).get(4).getText());
+
+		//スクリーンショットを撮影
+		getEvidence(new Object() {
+		});
 	}
 
 }
