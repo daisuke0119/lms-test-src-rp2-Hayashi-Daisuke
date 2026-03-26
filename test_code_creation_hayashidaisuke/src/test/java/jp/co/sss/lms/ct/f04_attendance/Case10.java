3930c1707f85b2e644106d7dd7af245ef8a91397
edu <edu@B65DS.mshome.net>
2026-03-26 11:35:19 +0900

    add//Case10.javaに打刻操作のテストコードを追記
    ・タイトルを取得し画面遷移の認証を行う。
    期待値：ログイン｜LMS
    
    ・正しいIDとパスワードでログインを行い、遷移先の画面のタイトルを取得、検証する。
    期待値：コース詳細｜LMS
    
    ・ヘッダーの「勤怠」のリンクをクリック
    期待値：勤怠情報変更｜LMS
    
    ・「出勤」ボタンを押下
    期待値：2026年3月26日(木)の出勤時刻がテスト実施時刻と同じ
    
    ・「退勤」ボタンを押下
    期待値：2026年3月26日(木)の退勤時刻がテスト実施時刻と同じ

--- test_code_creation_hayashidaisuke/src/test/java/jp/co/sss/lms/ct/f04_attendance/Case10.java
+++ test_code_creation_hayashidaisuke/src/test/java/jp/co/sss/lms/ct/f04_attendance/Case10.java
@@ -1,6 +1,10 @@
 package jp.co.sss.lms.ct.f04_attendance;
 
 import static jp.co.sss.lms.ct.util.WebDriverUtils.*;
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
@@ -9,6 +13,7 @@ import org.junit.jupiter.api.MethodOrderer.OrderAnnotation;
 import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestMethodOrder;
+import org.openqa.selenium.By;
 
 /**
  * 結合テスト 勤怠管理機能
@@ -35,35 +40,110 @@ public class Case10 {
 	@Order(1)
 	@DisplayName("テスト01 トップページURLでアクセス")
 	void test01() {
-		// TODO ここに追加
+
+		//ログインページを表示
+		goTo("http://localhost:8080/lms");
+
+		//タイトルが正しいか検証
+		assertEquals("ログイン | LMS", webDriver.getTitle());
+
+		//スクリーンショットを撮影
+		getEvidence(new Object() {
+		});
+
 	}
 
 	@Test
 	@Order(2)
 	@DisplayName("テスト02 初回ログイン済みの受講生ユーザーでログイン")
 	void test02() {
-		// TODO ここに追加
+		//ログインIDを入力
+		webDriver.findElement(By.id("loginId")).sendKeys("StudentAA01");
+
+		//パスワードを入力
+		webDriver.findElement(By.id("password")).sendKeys("PasswordAA01");
+
+		//ログインボタンをクリック
+		webDriver.findElement(By.cssSelector(".btn.btn-primary")).click();
+
+		//タイトルが正しいか検証
+		assertEquals("コース詳細 | LMS", webDriver.getTitle());
+
+		//スクリーンショットを撮影
+		getEvidence(new Object() {
+		});
+
 	}
 
 	@Test
 	@Order(3)
 	@DisplayName("テスト03 上部メニューの「勤怠」リンクから勤怠管理画面に遷移")
 	void test03() {
-		// TODO ここに追加
+
+		//ヘッダーの「勤怠」リンクをクリック
+		webDriver.findElement(By.partialLinkText("勤怠")).click();
+
+		//ポップアップが表示されるので「OK」をクリック
+		webDriver.switchTo().alert().accept();
+
+		//ページが表示されるまで待機
+		pageLoadTimeout(10);
+
+		//正しい画面遷移が行われているかタイトルを取得し、検証
+		assertEquals("勤怠情報変更｜LMS", webDriver.getTitle());
+
+		//スクリーンショットを撮影
+		getEvidence(new Object() {
+		});
+
 	}
 
 	@Test
 	@Order(4)
 	@DisplayName("テスト04 「出勤」ボタンを押下し出勤時間を登録")
 	void test04() {
-		// TODO ここに追加
+
+		//「出勤」ボタンを押下
+		webDriver.findElement(By.cssSelector("[name = 'punchIn'].btn")).click();
+
+		//ポップアップが表示されるため「OK」をクリック
+		webDriver.switchTo().alert().accept();
+
+		//正しい時刻(テスト実施時刻)が登録されているか検証
+		String targetDate = "2026年3月26日(木)";
+		String xpath = "//td[contains(text(),'" + targetDate + "')]/parent::tr/td[3]";
+		String expectedTime = LocalDateTime.now().format(DateTimeFormatter.ofPattern("HH:mm"));
+
+		assertEquals(expectedTime, webDriver.findElement(By.xpath(xpath)).getText());
+
+		//スクリーンショットを撮影
+		getEvidence(new Object() {
+		});
+
 	}
 
 	@Test
 	@Order(5)
 	@DisplayName("テスト05 「退勤」ボタンを押下し退勤時間を登録")
 	void test05() {
-		// TODO ここに追加
+
+		//「出勤」ボタンを押下
+		webDriver.findElement(By.cssSelector("[name = 'punchOut'].btn")).click();
+
+		//ポップアップが表示されるため「OK」をクリック
+		webDriver.switchTo().alert().accept();
+
+		//正しい時刻(テスト実施時刻)が登録されているか検証
+		String targetDate = "2026年3月26日(木)";
+		String xpath = "//td[contains(text(),'" + targetDate + "')]/parent::tr/td[4]";
+		String expectedTime = LocalDateTime.now().format(DateTimeFormatter.ofPattern("HH:mm"));
+
+		assertEquals(expectedTime, webDriver.findElement(By.xpath(xpath)).getText());
+
+		//スクリーンショットを撮影
+		getEvidence(new Object() {
+		});
+
 	}
 
 }
