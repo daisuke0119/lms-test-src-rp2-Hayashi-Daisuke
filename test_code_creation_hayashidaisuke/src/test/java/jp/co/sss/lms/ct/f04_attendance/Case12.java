126a208188cad8019f296c34461834737114351a
edu <edu@B65DS.mshome.net>
2026-03-27 11:10:09 +0900

    add//Case12.javaに勤怠情報を直接編集する際の入力チェックのテストコードを追記
    ・タイトルを取得し画面遷移の認証を行う。
    期待値：ログイン｜LMS
    
    ・正しいIDとパスワードでログインを行い、遷移先の画面のタイトルを取得、検証する。
    期待値：コース詳細｜LMS
    
    ・ヘッダーの「勤怠」のリンクをクリック
    期待値：勤怠情報変更｜LMS
    
    ・「勤怠情報を直接編集する」リンクをクリック
    期待値：「定時」ボタンが表示されている
    
    〇2026年3月25日(水)の勤怠情報において以下の入力チェックを行う
    ・出勤時間 (空白):00　退勤時間 18:(空白)
    エラーメッセージ：* 出勤時間が正しく入力されていません。
    　　　　　　　　　* 退勤時間が正しく入力されていません。
    
    ・出勤時間 (空白):(空白)　退勤時間 18:00
    エラーメッセージ：* 出勤情報がないため退勤情報を入力できません。
    
    ・出勤時間 19:00　退勤時間 18:00
    エラーメッセージ：* 退勤時刻[0]は出勤時刻[0]よりも後でなければいけません。
    
    ・出勤時間　17:00　退勤時間　18:00　中抜け時間　2時間
    エラーメッセージ：* 中抜け時間が勤務時間を超えています。
    
    ・出勤時間　09:00　退勤時間　18:00
    備考欄に「a」を101回入力
    エラーメッセージ：* 備考の長さが最大値(100)を超えています。

--- test_code_creation_hayashidaisuke/src/test/java/jp/co/sss/lms/ct/f04_attendance/Case12.java
+++ test_code_creation_hayashidaisuke/src/test/java/jp/co/sss/lms/ct/f04_attendance/Case12.java
@@ -1,6 +1,7 @@
 package jp.co.sss.lms.ct.f04_attendance;
 
 import static jp.co.sss.lms.ct.util.WebDriverUtils.*;
+import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
@@ -9,6 +10,8 @@ import org.junit.jupiter.api.MethodOrderer.OrderAnnotation;
 import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestMethodOrder;
+import org.openqa.selenium.By;
+import org.openqa.selenium.support.ui.Select;
 
 /**
  * 結合テスト 勤怠管理機能
@@ -35,63 +38,258 @@ public class Case12 {
 	@Order(1)
 	@DisplayName("テスト01 トップページURLでアクセス")
 	void test01() {
-		// TODO ここに追加
+
+		//ログインページを表示
+		goTo("http://localhost:8080/lms");
+
+		//タイトルが正しいか検証
+		assertEquals("ログイン | LMS", webDriver.getTitle());
+
+		//スクリーンショットを撮影
+		getEvidence(new Object() {
+		});
 	}
 
 	@Test
 	@Order(2)
 	@DisplayName("テスト02 初回ログイン済みの受講生ユーザーでログイン")
 	void test02() {
-		// TODO ここに追加
+
+		//ログインIDを入力
+		webDriver.findElement(By.id("loginId")).sendKeys("StudentAA01");
+
+		//パスワードを入力
+		webDriver.findElement(By.id("password")).sendKeys("PasswordAA01");
+
+		//ログインボタンをクリック
+		webDriver.findElement(By.cssSelector(".btn.btn-primary")).click();
+
+		//タイトルが正しいか検証
+		assertEquals("コース詳細 | LMS", webDriver.getTitle());
+
+		//スクリーンショットを撮影
+		getEvidence(new Object() {
+		});
 	}
 
 	@Test
 	@Order(3)
 	@DisplayName("テスト03 上部メニューの「勤怠」リンクから勤怠管理画面に遷移")
 	void test03() {
-		// TODO ここに追加
+
+		//ヘッダーの「勤怠」リンクをクリック
+		webDriver.findElement(By.partialLinkText("勤怠")).click();
+
+		//ポップアップが表示されるので「OK」をクリック
+		webDriver.switchTo().alert().accept();
+
+		//ページが表示されるまで待機
+		pageLoadTimeout(10);
+
+		//正しい画面遷移が行われているかタイトルを取得し、検証
+		assertEquals("勤怠情報変更｜LMS", webDriver.getTitle());
+
+		//スクリーンショットを撮影
+		getEvidence(new Object() {
+		});
 	}
 
 	@Test
 	@Order(4)
 	@DisplayName("テスト04 「勤怠情報を直接編集する」リンクから勤怠情報直接変更画面に遷移")
 	void test04() {
-		// TODO ここに追加
+
+		//「勤怠情報を直接編集する」リンクをクリックする
+		webDriver.findElement(By.partialLinkText("勤怠情報を直接編集する")).click();
+
+		//ページが表示されるまで待機
+		pageLoadTimeout(10);
+
+		//「定時」ボタンが表示されているかどうか検証
+		assertTrue(webDriver.findElement(By.cssSelector("button.btn.btn-success")).getText().contains("定時"));
+
+		//スクリーンショットを撮影
+		getEvidence(new Object() {
+		});
 	}
 
 	@Test
 	@Order(5)
 	@DisplayName("テスト05 不適切な内容で修正してエラー表示：出退勤の（時）と（分）のいずれかが空白")
 	void test05() {
-		// TODO ここに追加
+
+		//2026年3月25日(水)の出退勤の項目に不適切な入力を行う
+		Select startH = new Select(webDriver.findElement(By.id("startHour0")));
+		Select startM = new Select(webDriver.findElement(By.id("startMinute0")));
+		Select endH = new Select(webDriver.findElement(By.id("endHour0")));
+		Select endM = new Select(webDriver.findElement(By.id("endMinute0")));
+
+		startH.selectByIndex(0);
+		startM.selectByVisibleText("00");
+		endH.selectByVisibleText("18");
+		endM.selectByIndex(0);
+
+		//「更新」ボタンが表示されるまでスクロール
+		scrollBy("500");
+
+		//「更新」ボタンを押下
+		webDriver.findElement(By.cssSelector("input.btn[value = '更新']")).click();
+
+		//ポップアップが表示されるので「OK」をクリック
+		webDriver.switchTo().alert().accept();
+
+		//エラーメッセージが表示されているか検証
+		assertTrue(webDriver.findElement(By.cssSelector(".help-inline.error")).isDisplayed());
+
+		//表示されているエラーメッセージが正しいか検証
+		assertTrue(webDriver.findElements(By.cssSelector(".help-inline.error")).get(0).getText().contains("出勤時間"));
+		assertTrue(webDriver.findElements(By.cssSelector(".help-inline.error")).get(1).getText().contains("退勤時間"));
+		//スクリーンショットを撮影
+		getEvidence(new Object() {
+		});
 	}
 
 	@Test
 	@Order(6)
 	@DisplayName("テスト06 不適切な内容で修正してエラー表示：出勤が空白で退勤に入力あり")
 	void test06() {
-		// TODO ここに追加
+
+		//2026年3月25日(水)の出退勤の項目に不適切な入力を行う
+		Select startH = new Select(webDriver.findElement(By.id("startHour0")));
+		Select startM = new Select(webDriver.findElement(By.id("startMinute0")));
+		Select endH = new Select(webDriver.findElement(By.id("endHour0")));
+		Select endM = new Select(webDriver.findElement(By.id("endMinute0")));
+
+		startH.selectByIndex(0);
+		startM.selectByIndex(0);
+		endH.selectByVisibleText("18");
+		endM.selectByVisibleText("00");
+
+		//「更新」ボタンが表示されるまでスクロール
+		scrollBy("500");
+
+		//「更新」ボタンを押下
+		webDriver.findElement(By.cssSelector("input.btn[value = '更新']")).click();
+
+		//ポップアップが表示されるので「OK」をクリック
+		webDriver.switchTo().alert().accept();
+
+		//エラーメッセージが表示されているか検証
+		assertTrue(webDriver.findElement(By.cssSelector(".help-inline.error")).isDisplayed());
+
+		//表示されているエラーメッセージが正しいか検証
+		assertTrue(webDriver.findElement(By.cssSelector(".help-inline.error")).getText().contains("出勤情報"));
+
+		//スクリーンショットを撮影
+		getEvidence(new Object() {
+		});
 	}
 
 	@Test
 	@Order(7)
 	@DisplayName("テスト07 不適切な内容で修正してエラー表示：出勤が退勤よりも遅い時間")
 	void test07() {
-		// TODO ここに追加
+
+		//2026年3月25日(水)の出退勤の項目に不適切な入力を行う
+		Select startH = new Select(webDriver.findElement(By.id("startHour0")));
+		Select startM = new Select(webDriver.findElement(By.id("startMinute0")));
+		Select endH = new Select(webDriver.findElement(By.id("endHour0")));
+		Select endM = new Select(webDriver.findElement(By.id("endMinute0")));
+
+		startH.selectByVisibleText("19");
+		startM.selectByVisibleText("00");
+		endH.selectByVisibleText("18");
+		endM.selectByVisibleText("00");
+
+		//「更新」ボタンが表示されるまでスクロール
+		scrollBy("500");
+
+		//「更新」ボタンを押下
+		webDriver.findElement(By.cssSelector("input.btn[value = '更新']")).click();
+
+		//ポップアップが表示されるので「OK」をクリック
+		webDriver.switchTo().alert().accept();
+
+		//エラーメッセージが表示されているか検証
+		assertTrue(webDriver.findElement(By.cssSelector(".help-inline.error")).isDisplayed());
+
+		//表示されているエラーメッセージが正しいか検証
+		assertTrue(webDriver.findElement(By.cssSelector(".help-inline.error")).getText().contains("出勤時刻"));
+		assertTrue(webDriver.findElement(By.cssSelector(".help-inline.error")).getText().contains("退勤時刻"));
+
+		//スクリーンショットを撮影
+		getEvidence(new Object() {
+		});
 	}
 
 	@Test
 	@Order(8)
 	@DisplayName("テスト08 不適切な内容で修正してエラー表示：出退勤時間を超える中抜け時間")
 	void test08() {
-		// TODO ここに追加
+		//2026年3月25日(水)の出退勤、中抜けの項目に不適切な入力を行う
+		Select startH = new Select(webDriver.findElement(By.id("startHour0")));
+		Select startM = new Select(webDriver.findElement(By.id("startMinute0")));
+		Select endH = new Select(webDriver.findElement(By.id("endHour0")));
+		Select endM = new Select(webDriver.findElement(By.id("endMinute0")));
+		Select stepOutTime = new Select(webDriver.findElement(By.name("attendanceList[0].blankTime")));
+
+		startH.selectByVisibleText("17");
+		startM.selectByVisibleText("00");
+		endH.selectByVisibleText("18");
+		endM.selectByVisibleText("00");
+		stepOutTime.selectByVisibleText("2時間");
+
+		//「更新」ボタンが表示されるまでスクロール
+		scrollBy("500");
+
+		//「更新」ボタンを押下
+		webDriver.findElement(By.cssSelector("input.btn[value = '更新']")).click();
+
+		//ポップアップが表示されるので「OK」をクリック
+		webDriver.switchTo().alert().accept();
+
+		//エラーメッセージが表示されているか検証
+		assertTrue(webDriver.findElement(By.cssSelector(".help-inline.error")).isDisplayed());
+
+		//表示されているエラーメッセージが正しいか検証
+		assertTrue(webDriver.findElement(By.cssSelector(".help-inline.error")).getText().contains("中抜け時間"));
+
+		//スクリーンショットを撮影
+		getEvidence(new Object() {
+		});
 	}
 
 	@Test
 	@Order(9)
 	@DisplayName("テスト09 不適切な内容で修正してエラー表示：備考が100文字超")
 	void test09() {
-		// TODO ここに追加
+
+		String wrongText = "a".repeat(101);
+
+		//2026年3月25日(水)の出退勤の時刻を「定時」ボタンで入力
+		webDriver.findElements(By.cssSelector("button.btn.btn-success")).get(0).click();
+
+		//備考欄に不適切な内容を入力
+		webDriver.findElement(By.name("attendanceList[0].note")).sendKeys(wrongText);
+
+		//「更新」ボタンが表示されるまでスクロール
+		scrollBy("500");
+
+		//「更新」ボタンを押下
+		webDriver.findElement(By.cssSelector("input.btn[value = '更新']")).click();
+
+		//ポップアップが表示されるので「OK」をクリック
+		webDriver.switchTo().alert().accept();
+
+		//エラーメッセージが表示されているか検証
+		assertTrue(webDriver.findElement(By.cssSelector(".help-inline.error")).isDisplayed());
+
+		//表示されているエラーメッセージが正しいか検証
+		assertTrue(webDriver.findElement(By.cssSelector(".help-inline.error")).getText().contains("備考の長さ"));
+
+		//スクリーンショットを撮影
+		getEvidence(new Object() {
+		});
 	}
 
 }
